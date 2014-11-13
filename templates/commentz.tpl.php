7bf89e4100fa00764386d771fc43e1a2bf36bc51
unknown <e7jwu1d@ACNU301B1RP.na.corp.mckesson.com>
2014-11-13 17:25:55 -0600

    Ajax delete and styling

--- templates/commentz.tpl.php
+++ templates/commentz.tpl.php
@@ -5,32 +5,35 @@
  * Default theme implementation for comments.
  */
 ?>
-<div class="comment-inner-<?php print $comment->cid; ?> clearfix"
-<?php  print $attributes; ?>>
-  <div class="comment-body">
-  <?php  if(variable_get('better_comments_picture') == 1): ?>
-    <?php print $picture; ?>
-<?php endif;?>
-  <?php print render($title_preffix); ?>
-
-  <div class="comment-content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-    hide($content['links']);
-    // Print username.
-    print $author;
-    print render($content);?>
-
-     <div class="comment-data">
-   <span><?php print $created; ?></span>
-   <span><?php print render($content['links']) ?></span>
-
-    <?php if ($signature): ?>
-    <div class="user-signature clearfix">
-      <?php print $signature ?>
+<div class="comment-inner-<?php print $comment->cid; ?> clearfix"<?php  print $attributes; ?>>
+  <div class="commentz-body">
+    <div class='commentz-header'>
+      <div class='commentz-header-avatar'><?php print $picture; ?></div>
+      <div class='commentz-header-info'>
+        <div><?php print $author; ?></div>
+        <div class='commentz-header-created'><?php print $created; ?> </div>
+      </div>
+      <?php print render($title_preffix); ?>
+      <div class='clearfix'></div>
     </div>
-    <?php endif; ?>
-  </div>
-   </div>
+    <div class="commentz-content"<?php print $content_attributes; ?>>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['links']);
+        print render($content);
+      ?>
+
+      <div class="commentz-data">
+
+        <span><?php print render($content['links']) ?></span>
+
+        <?php if ($signature): ?>
+          <div class="user-signature clearfix">
+            <?php print $signature ?>
+          </div>
+        <?php endif; ?>
+
+      </div>
+    </div>
   </div>
 </div>
